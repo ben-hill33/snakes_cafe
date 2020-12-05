29323509fec48ee7398e307793284108a8a01f9e
BEN <benjamin.hill28@gmail.com>
2020-12-05 15:08:50 -0800

    updated readme, added menu to snakes_cafe

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -4,6 +4,43 @@ orders = {
 
 print(orders)
 
-print('**************************************')
-print('**   Welcome to the Snakes Cafe!    **')
-print('**************************************')
+menu = """
+**************************************
+**   Welcome to the Snakes Cafe!    **
+**   Please see our menu below.     **
+**                                  ** 
+** To quit at any time, type "quit" **
+**************************************
+
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+
+**************************************
+**   What would you like to order?  **
+**************************************
+
+"""
+
+print(menu)
