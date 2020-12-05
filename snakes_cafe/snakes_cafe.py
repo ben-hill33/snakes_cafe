48f3a29552534f5a9393a16ea1315c9f4c478d26
BEN <benjamin.hill28@gmail.com>
2020-12-05 14:17:07 -0800

    proof of life test

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,1 +1,9 @@
-print('Proof of life')
+orders = {
+    'wings': 0,
+}
+
+print(orders)
+
+print('**************************************')
+print('**   Welcome to the Snakes Cafe!    **')
+print('**************************************')
