0ac2d6666fd689de4bacb5a0976f3ddcf0914d38
BEN <benjamin.hill28@gmail.com>
2020-12-06 19:06:55 -0800

    getting type error string indices must be integers

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,39 +1,71 @@
 import menu
 
 
-appetizers = {
-    'wings': 0,
-    # 'onion_rings': 0,
-    # 'spring_rolls': 0,
-}
-
-entrees = {
-    'wings': 0,
-    # 'onion_rings': 0,
-    # 'spring_rolls': 0,
-}
-
-desserts = {
-    'wings': 0,
-    # 'onion_rings': 0,
-    # 'spring_rolls': 0,
-}
-
-
 def main():
     print(menu.menu)
+    choice = {
+        'wings': 0,
+        'onion_rings': 0,
+        'spring_rolls': 0,
+        'salmon': 0,
+        'steak': 0,
+        'meat_tornado': 0,
+        'a_literal_garden': 0,
+        'ice_cream': 0,
+        'cake': 0,
+        'pie': 0,
+        'cookies': 0,
+        'beer': 0,
+        'soda': 0,
+        'coffee': 0,
+        'tea': 0,
+        'unicorn_tears': 0,
+        'quit': 'quit',
+    }
 
-    # print(menu)
-    appetizer_order = appetizers['wings']
+    while choice != 'quit':
+        choice = input('> ')
+        if choice == 'wings':
+            # print()
+            # choice['wings'] += 1
+            return (str(choice['wings']) +
+                    '** order of Wings have been added to your meal **')
+        elif choice == 'quit':
+            print('Thank you for your order!')
+        else:
+            print('Try again')
 
-    user_input = input('> ')
-
-    print(user_input)
-    print(appetizer_order)
-
-    appetizers['wings'] += 1
-    print(appetizers)
+    # print(user_input)
 
 
 if __name__ == "__main__":
     main()
+
+
+# appetizers = {
+#     'wings': 0,
+#     'onion_rings': 0,
+#     'spring_rolls': 0,
+# }
+
+# entrees = {
+#     'salmon': 0,
+#     'steak': 0,
+#     'meat_tornado': 0,
+#     'a_literal_garden': 0,
+# }
+
+# desserts = {
+#     'ice_cream': 0,
+#     'cake': 0,
+#     'pie': 0,
+#     'cookies': 0,
+# }
+
+# beverages = {
+#     'beer': 0,
+#     'soda': 0,
+#     'coffee': 0,
+#     'tea': 0,
+#     'unicorn_tears': 0,
+# }
